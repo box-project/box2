921e2be8923867682f179a71a864ab94ad1fd0cd
Kevin Herrera <me@kevingh.com>
2012-07-29 09:04:29 -0700

    Missed a file.

--- src/lib/KevinGH/Box/Console/Command/Update.php
+++ src/lib/KevinGH/Box/Console/Command/Update.php
@@ -1,59 +1,55 @@
 <?php
 
-    /* This file is part of Box.
-     *
-     * (c) 2012 Kevin Herrera
-     *
-     * For the full copyright and license information, please
-     * view the LICENSE file that was distributed with this
-     * source code.
-     */
+/* This file is part of Box.
+ *
+ * (c) 2012 Kevin Herrera
+ *
+ * For the full copyright and license information, please
+ * view the LICENSE file that was distributed with this
+ * source code.
+ */
 
-    namespace KevinGH\Box\Console\Command;
+namespace KevinGH\Box\Console\Command;
 
-    use KevinGH\Amend\Command,
-        Phar,
-        RuntimeException,
-        Symfony\Component\Console\Input\InputInterface,
-        Symfony\Component\Console\Output\OutputInterface,
-        UnexpectedValueException;
+use KevinGH\Amend\Command,
+    Phar,
+    RuntimeException,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    UnexpectedValueException;
+
+/** {@inheritDoc} */
+class Update extends Command
+{
+    /** {@inheritDoc} */
+    protected $extract = '@update_matcher@';
 
     /** {@inheritDoc} */
-    class Update extends Command
+    protected $match = '@update_matcher@';
+
+    /** {@inheritDoc} */
+    protected $url = '@update_url@';
+
+    /** {@inheritDoc} */
+    public function __construct()
     {
-        /** {@inheritDoc} */
-        protected $extract = '@update_matcher@';
-
-        /** {@inheritDoc} */
-        protected $match = '@update_matcher@';
-
-        /** {@inheritDoc} */
-        protected $url = '@update_url@';
-
-        /** {@inheritDoc} */
-        public function __construct()
-        {
-            parent::__construct('update');
-        }
-
-        /** {@inheritDoc} */
-        public function execute(InputInterface $input, OutputInterface $output)
-        {
-            $this->integrity = function ($file) use ($output)
-            {
-                try
-                {
-                    $phar = new Phar($file);
-                }
-
-                catch (UnexpectedValueException $e)
-                {
-                    $output->writeln("<error>The update was corrupted.</error>\n");
-
-                    throw $e;
-                }
-            };
-
-            return parent::execute($input, $output);
-        }
+        parent::__construct('update');
     }
+
+    /** {@inheritDoc} */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->integrity = function ($file) use ($output)
+        {
+            try {
+                $phar = new Phar($file);
+            } catch (UnexpectedValueException $e) {
+                $output->writeln("<error>The update was corrupted.</error>\n");
+
+                throw $e;
+            }
+        };
+
+        return parent::execute($input, $output);
+    }
+}
