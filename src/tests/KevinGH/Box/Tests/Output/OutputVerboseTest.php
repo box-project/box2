23f18cf1107151c97acc2b724b4309a0017ab0a9
Kevin Herrera <kevin@herrera.io>
2013-03-15 23:50:56 -0700

    Adding output handler filtered by verbosity.

---
+++ src/tests/KevinGH/Box/Tests/Output/OutputVerboseTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace KevinGH\Box\Tests\Output;
+
+use Herrera\PHPUnit\TestCase;
+use KevinGH\Box\Output\OutputVerbose;
+
+class OutputVerboseTest extends TestCase
+{
+    public function testVerbose()
+    {
+        $output = $this->getMock(
+            'KevinGH\\Box\\Output\\OutputVerbose',
+            array('write')
+        );
+
+        $output->expects($this->once())
+               ->method('write')
+               ->with(
+                     $this->equalto('Hello!'),
+                     $this->isFalse(),
+                     $this->equalTo(0)
+                 );
+
+        $output->verbose('Test!');
+        $output->setVerbosity(OutputVerbose::VERBOSITY_VERBOSE);
+        $output->verbose('Hello!');
+    }
+
+    public function testVerboseln()
+    {
+        $output = $this->getMock(
+            'KevinGH\\Box\\Output\\OutputVerbose',
+            array('write')
+        );
+
+        $output->expects($this->once())
+               ->method('write')
+               ->with(
+                   $this->equalto('Hello!'),
+                   $this->isTrue(),
+                   $this->equalTo(0)
+                 );
+
+        $output->verboseln('Test!');
+        $output->setVerbosity(OutputVerbose::VERBOSITY_VERBOSE);
+        $output->verboseln('Hello!');
+    }
+}
