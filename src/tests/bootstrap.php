fddd4040bc777dc706a2a808dafe2a215a989dd3
Kevin Herrera <kevin@herrera.io>
2013-03-18 14:06:08 -0700

    Adding test directory to autoloader.

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
-require __DIR__ .'/../vendors/autoload.php';
+$loader = require __DIR__ .'/../vendors/autoload.php';
+$loader->add(null, __DIR__);
 
 define('BOX_PATH', realpath(__DIR__) . '/../..');
