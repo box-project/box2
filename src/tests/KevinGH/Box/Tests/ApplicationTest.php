c38585087ea54e1166a8ce439cc14ef553d6a2af
Kevin Herrera <kevin@herrera.io>
2013-03-19 08:25:53 -0700

    Adding Application class test.

---
+++ src/tests/KevinGH/Box/Tests/ApplicationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace KevinGH\Box\Tests;
+
+use ErrorException;
+use Herrera\PHPUnit\TestCase;
+use KevinGH\Box\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\StreamOutput;
+
+class ApplicationTest extends TestCase
+{
+    public function testApp()
+    {
+        $app = new Application();
+        $app->setAutoExit(false);
+
+        $input = new ArrayInput(array('--version'));
+        $stream = fopen('php://memory', 'w', false);
+        $output = new StreamOutput($stream);
+
+        $app->run($input, $output);
+
+        rewind($stream);
+
+        $string = trim(fgets($stream));
+        $string = preg_replace(
+            array(
+                '/\x1b(\[|\(|\))[;?0-9]*[0-9A-Za-z]/',
+                '/\x1b(\[|\(|\))[;?0-9]*[0-9A-Za-z]/',
+                '/[\x03|\x1a]/'
+            ),
+            array('', '', ''),
+            $string
+        );
+
+        $this->assertEquals('Box version @git_tag@', $string);
+
+        try {
+            trigger_error('Test.', E_USER_WARNING);
+        } catch (ErrorException $exception) {
+        }
+
+        $this->assertTrue(isset($exception));
+    }
+}
