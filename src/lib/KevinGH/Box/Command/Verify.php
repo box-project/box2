6cd827376e53fb0c9736cff209ab20320fc93b34
Kevin Herrera <kevin@herrera.io>
2013-07-23 14:01:03 -0700

    Adding support for extension-less verification.

--- src/lib/KevinGH/Box/Command/Verify.php
+++ src/lib/KevinGH/Box/Command/Verify.php
@@ -3,10 +3,12 @@
 namespace KevinGH\Box\Command;
 
 use Exception;
+use Herrera\Box\Signature;
 use Phar;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -26,12 +28,37 @@ class Verify extends Command
         $this->setHelp(
             <<<HELP
 The <info>%command.name%</info> command will verify the signature of the Phar.
+
+By default, the command will use the <comment>phar</comment> extension to perform the
+verification process. However, if the extension is not available,
+Box will manually extract and verify the phar's signature. If you
+require that Box handle the verification process, you will need
+to use the <comment>--no-extension</comment> option.
+
+<question>Why would I require that box handle the verification process?</question>
+
+If you meet all of the following conditions:
+ - the <comment>phar</comment> extension installed
+ - the <comment>openssl</comment> extension is not installed
+ - you need to verify a phar signed using a private key
+
+Box supports verifying private key signed phars without using
+either extension. <error>Note however, that the entire phar will need
+to be read into memory before the verification can be performed.</error>
+The library used for handling the verification process does not
+currently support progressive/incremental hashing.
 HELP
         );
         $this->addArgument(
             'phar',
             InputArgument::REQUIRED,
             'The Phar file.'
+        );
+        $this->addOption(
+            'no-extension',
+            null,
+            InputOption::VALUE_NONE,
+            'Do not use the phar extension to verify.'
         );
     }
 
@@ -58,25 +85,37 @@ HELP
             return 1;
         }
 
-        try {
-            $phar = new Phar($input->getArgument('phar'));
+        $signature = null;
+        $verified = false;
 
+        try {
+            if (!$input->getOption('no-extension') && extension_loaded('phar')) {
+                $phar = new Phar($phar);
+                $verified = true;
+                $signature = $phar->getSignature();
+            } else {
+                $phar = new Signature($phar);
+                $verified = $phar->verify();
+                $signature = $phar->get();
+            }
+        } catch (Exception $exception) {
+        }
+
+        if ($verified) {
             $output->writeln('<info>The Phar passed verification.</info>');
 
             if ($verbose) {
-                $signature = $phar->getSignature();
-
                 $output->writeln($signature['hash_type'] . ' Signature:');
                 $output->writeln($signature['hash']);
             }
-        } catch (Exception $exception) {
-            $output->writeln('<error>The Phar failed verification.</error>');
-
-            if ($verbose) {
-                throw $exception;
-            }
 
             return 1;
+        }
+
+        $output->writeln('<error>The Phar failed verification.</error>');
+
+        if ($verbose && isset($exception)) {
+            throw $exception;
         }
 
         return 0;
--- src/tests/KevinGH/Box/Tests/Command/VerifyTest.php
+++ src/tests/KevinGH/Box/Tests/Command/VerifyTest.php
@@ -10,7 +10,7 @@ use UnexpectedValueException;
 
 class VerifyTest extends CommandTestCase
 {
-    public function testExecute()
+    public function testExecuteExtension()
     {
         file_put_contents('test.php', '<?php echo "Hello!";');
 
@@ -26,6 +26,40 @@ class VerifyTest extends CommandTestCase
             array(
                 'command' => 'verify',
                 'phar' => 'test.phar'
+            ),
+            array(
+                'verbosity' => OutputInterface::VERBOSITY_VERBOSE
+            )
+        );
+
+        $expected = <<<OUTPUT
+Verifying the Phar...
+The Phar passed verification.
+{$signature['hash_type']} Signature:
+{$signature['hash']}
+
+OUTPUT;
+
+        $this->assertEquals($expected, $this->getOutput($tester));
+    }
+
+    public function testExecuteLibrary()
+    {
+        file_put_contents('test.php', '<?php echo "Hello!";');
+
+        $phar = new Phar('test.phar');
+        $phar->addFile('test.php', 'test.php');
+
+        $signature = $phar->getSignature();
+
+        unset($phar);
+
+        $tester = $this->getTester();
+        $tester->execute(
+            array(
+                'command' => 'verify',
+                'phar' => 'test.phar',
+                '--no-extension' => true,
             ),
             array(
                 'verbosity' => OutputInterface::VERBOSITY_VERBOSE
