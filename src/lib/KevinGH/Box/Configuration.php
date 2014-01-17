41ecc30430548d479c7b3ce4ff7f78d8f6f93092
Peter Hansen <peter.hansen@rwth-aachen.de>
2014-01-17 18:11:10 +0100

    removed whitespaces

--- src/lib/KevinGH/Box/Configuration.php
+++ src/lib/KevinGH/Box/Configuration.php
@@ -592,7 +592,7 @@ class Configuration
             $path = $this->getBasePath() . DIRECTORY_SEPARATOR . $path;
 
             if (false === ($contents = @file_get_contents($path))) {
-                $errors = error_get_last();               
+                $errors = error_get_last();
                 if ($errors === null) {
                     $errors = array('message' => 'failed to get contents of \''.$path.'\'');
                 }
