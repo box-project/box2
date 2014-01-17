7fe558c898c0bd98f10d7ca0954fee762140d41a
Kevin Herrera <kevin@herrera.io>
2014-01-16 16:23:44 -0800

    Adding current configuration definition. (incomplete)

---
+++ src/lib/KHerGe/Box/Config/Definition.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace KHerGe\Box\Config;
+
+use Phar;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Defines the expected configuration tree for normalization and validation.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class Definition implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $builder = new TreeBuilder();
+        $root = $builder->root('box');
+
+        $this->buildSources($root);
+        $this->buildStub($root);
+
+        return $builder;
+    }
+
+    /**
+     * Builds the sources configuration tree.
+     *
+     * This part of the configuration tree builder will define the possible
+     * sources used to build the archive, as well as some default values for
+     * those options. With the default options, we should be able to simply
+     * iterate through each source and use its value.
+     *
+     * @param ArrayNodeDefinition|NodeDefinition $root The root node.
+     */
+    private function buildSources($root)
+    {
+        $root
+            ->children()
+                ->arrayNode('sources')
+                    ->children()
+
+                        // base directory path
+                        ->scalarNode('base')->defaultNull()->end()
+
+                        // directories
+                        ->arrayNode('dirs')
+                            ->prototype('array')
+                                ->beforeNormalization()
+                                    ->ifString()->then(
+                                        function ($value) {
+                                            return array(
+                                                'extension' => array('php'),
+                                                'path' => $value
+                                            );
+                                        }
+                                    )
+                                ->end()
+                                ->children()
+                                    ->arrayNode('extension')
+                                        ->beforeNormalization()
+                                            ->ifString()->then(function ($value) { return array($value); })
+                                        ->end()
+                                        ->prototype('scalar')
+                                            ->isRequired()
+                                            ->cannotBeEmpty()
+                                        ->end()
+                                        ->defaultValue(array('php'))
+                                    ->end()
+                                    ->scalarNode('filter')->defaultNull()->end()
+                                    ->arrayNode('ignore')
+                                        ->beforeNormalization()
+                                            ->ifString()->then(function ($value) { return array($value); })
+                                        ->end()
+                                        ->prototype('scalar')
+                                            ->isRequired()
+                                            ->cannotBeEmpty()
+                                        ->end()
+                                    ->end()
+                                    ->scalarNode('path')->isRequired()->cannotBeEmpty()->end()
+                                    ->scalarNode('rename')->defaultNull()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                        // files
+                        ->arrayNode('files')
+                            ->prototype('array')
+                                ->beforeNormalization()
+                                    ->ifString()->then(function ($value) { return array('path' => $value); })
+                                ->end()
+                                ->children()
+                                    ->scalarNode('path')->isRequired()->cannotBeEmpty()->end()
+                                    ->scalarNode('rename')->defaultNull()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                    ->end()
+                ->end()
+            ->end();
+    }
+
+    /**
+     * Builds the stub configuration tree.
+     *
+     * This part of the tree builder will define the possible stub configuration
+     * options, as well as some of the default values for those options. With
+     * the default options, we should be able to simply iterate through each
+     * configuration option and use its value.
+     *
+     * @param ArrayNodeDefinition|NodeDefinition $root The root node.
+     */
+    private function buildStub($root)
+    {
+        $root
+            ->children()
+                ->arrayNode('stub')
+                    ->children()
+
+                        // addRequire()
+                        ->arrayNode('require')
+                            ->prototype('array')
+                                ->beforeNormalization()
+                                    ->ifString()->then(function ($value) { return array('file' => $value); })
+                                ->end()
+                                ->children()
+                                    ->scalarNode('file')->isRequired()->cannotBeEmpty()->end()
+                                    ->booleanNode('internal')->defaultTrue()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                        // addSource()
+                        ->arrayNode('source')
+                            ->prototype('array')
+                                ->beforeNormalization()
+                                    ->ifString()->then(function ($value) { return array('source' => $value); })
+                                ->end()
+                                ->children()
+                                    ->booleanNode('after')->defaultTrue()->end()
+                                    ->scalarNode('source')->isRequired()->cannotBeEmpty()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                        // interceptFileFuncs()
+                        ->booleanNode('intercept')->defaultFalse()->end()
+
+                        // loadPhar()
+                        ->arrayNode('load')
+                            ->prototype('array')
+                                ->beforeNormalization()
+                                    ->ifString()->then(function ($value) { return array('file' => $value); })
+                                ->end()
+                                ->children()
+                                    ->scalarNode('alias')->defaultNull()->end()
+                                    ->scalarNode('file')->isRequired()->cannotBeEmpty()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                        // mapPhar()
+                        ->scalarNode('map')->defaultNull()->end()
+
+                        // mount()
+                        ->arrayNode('mount')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('external')->isRequired()->cannotBeEmpty()->end()
+                                    ->scalarNode('internal')->isRequired()->cannotBeEmpty()->end()
+                                ->end()
+                            ->end()
+                        ->end()
+
+                        // mungServer()
+                        ->arrayNode('mung')
+                            ->prototype('enum')
+                                ->values(
+                                    array(
+                                        'PHP_SELF',
+                                        'REQUEST_URI',
+                                        'SCRIPT_FILENAME',
+                                        'SCRIPT_NAME'
+                                    )
+                                )
+                            ->end()
+                        ->end()
+
+                        // selfExtracting()
+                        ->booleanNode('extractable')->defaultFalse()->end()
+
+                        // setBanner()
+                        ->scalarNode('banner')->defaultNull()->end()
+
+                        // setShebang()
+                        ->scalarNode('shebang')->defaultValue('#!/usr/bin/env php')->end()
+
+                        // webPhar()
+                        ->arrayNode('web')
+                            ->children()
+                                ->scalarNode('alias')->defaultNull()->end()
+                                ->scalarNode('index')->defaultValue('index.php')->end()
+                                ->scalarNode('not_found')->defaultNull()->end()
+                                ->arrayNode('mime')
+                                    ->useAttributeAsKey('extension')
+                                    ->prototype('scalar')
+                                        ->isRequired()
+                                        ->cannotBeEmpty()
+                                    ->end()
+                                    ->defaultValue(
+                                        array(
+                                            'avi' => 'video/avi',
+                                            'bmp' => 'image/bmp',
+                                            'c' => 'text/plain',
+                                            'c++' => 'text/plain',
+                                            'cc' => 'text/plain',
+                                            'cpp' => 'text/plain',
+                                            'css' => 'text/css',
+                                            'dtd' => 'text/plain',
+                                            'gif' => 'image/gif',
+                                            'h' => 'text/plain',
+                                            'htm' => 'text/html',
+                                            'html' => 'text/html',
+                                            'htmls' => 'text/html',
+                                            'ico' => 'image/x-ico',
+                                            'inc' => Phar::PHP,
+                                            'jpe' => 'image/jpeg',
+                                            'jpeg' => 'image/jpeg',
+                                            'jpg' => 'image/jpeg',
+                                            'js' => 'application/x-javascript',
+                                            'log' => 'text/plain',
+                                            'mid' => 'audio/midi',
+                                            'midi' => 'audio/midi',
+                                            'mod' => 'audio/mod',
+                                            'mov' => 'movie/quicktime',
+                                            'mp3' => 'audio/mp3',
+                                            'mpeg' => 'video/mpeg',
+                                            'mpg' => 'video/mpeg',
+                                            'pdf' => 'application/pdf',
+                                            'php' => Phar::PHP,
+                                            'phps' => Phar::PHPS,
+                                            'png' => 'image/png',
+                                            'rng' => 'text/plain',
+                                            'swf' => 'application/shockwave-flash',
+                                            'tif' => 'image/tiff',
+                                            'tiff' => 'image/tiff',
+                                            'txt' => 'text/plain',
+                                            'wav' => 'audio/wav',
+                                            'xbm' => 'image/xbm',
+                                            'xml' => 'text/xml',
+                                            'xsd' => 'text/plain',
+                                        )
+                                    )
+                                ->end()
+                                ->scalarNode('rewrite')->defaultNull()->end()
+                            ->end()
+                        ->end()
+
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
---
+++ src/tests/KHerGe/Box/Tests/Functional/Config/DefinitionTest.php
@@ -0,0 +1,424 @@
+<?php
+
+namespace KHerGe\Box\Tests\Functional\Config;
+
+use KHerGe\Box\Config\Definition;
+use Phar;
+use PHPUnit_Framework_TestCase as TestCase;
+use Symfony\Component\Config\Definition\Processor;
+
+/**
+ * Performs functional testing on `Definition`.
+ *
+ * @see KHerGe\Box\Config\Definition
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class DefinitionTest extends TestCase
+{
+    /**
+     * The definition instance being tested.
+     *
+     * @var Definition
+     */
+    private $definition;
+
+    /**
+     * The configuration processor.
+     *
+     * @var Processor
+     */
+    private $processor;
+
+    /**
+     * Make sure that the default empty source settings are set as expected.
+     */
+    public function testDefaultEmptySource()
+    {
+        $expected = array(
+            'sources' => array(
+                'base' => null,
+                'dirs' => array(),
+                'files' => array(),
+            )
+        );
+
+        $sources = array(
+            'sources' => array()
+        );
+
+        $this->assertEquals(
+            $expected,
+            $this->processor->processConfiguration(
+                $this->definition,
+                array($sources)
+            )
+        );
+    }
+
+    /**
+     * Make sure that the default empty stub settings are set as expected.
+     */
+    public function testDefaultEmptyStub()
+    {
+        $expected = array(
+            'stub' => array(
+                'web' => array(
+                    'alias' => null,
+                    'index' => 'index.php',
+                    'not_found' => null,
+                    'mime' => array(
+                        'avi' => 'video/avi',
+                        'bmp' => 'image/bmp',
+                        'c' => 'text/plain',
+                        'c++' => 'text/plain',
+                        'cc' => 'text/plain',
+                        'cpp' => 'text/plain',
+                        'css' => 'text/css',
+                        'dtd' => 'text/plain',
+                        'gif' => 'image/gif',
+                        'h' => 'text/plain',
+                        'htm' => 'text/html',
+                        'html' => 'text/html',
+                        'htmls' => 'text/html',
+                        'ico' => 'image/x-ico',
+                        'inc' => Phar::PHP,
+                        'jpe' => 'image/jpeg',
+                        'jpeg' => 'image/jpeg',
+                        'jpg' => 'image/jpeg',
+                        'js' => 'application/x-javascript',
+                        'log' => 'text/plain',
+                        'mid' => 'audio/midi',
+                        'midi' => 'audio/midi',
+                        'mod' => 'audio/mod',
+                        'mov' => 'movie/quicktime',
+                        'mp3' => 'audio/mp3',
+                        'mpeg' => 'video/mpeg',
+                        'mpg' => 'video/mpeg',
+                        'pdf' => 'application/pdf',
+                        'php' => Phar::PHP,
+                        'phps' => Phar::PHPS,
+                        'png' => 'image/png',
+                        'rng' => 'text/plain',
+                        'swf' => 'application/shockwave-flash',
+                        'tif' => 'image/tiff',
+                        'tiff' => 'image/tiff',
+                        'txt' => 'text/plain',
+                        'wav' => 'audio/wav',
+                        'xbm' => 'image/xbm',
+                        'xml' => 'text/xml',
+                        'xsd' => 'text/plain',
+                    ),
+                    'rewrite' => null,
+                ),
+                'require' => array(),
+                'source' => array(),
+                'intercept' => false,
+                'load' => array(),
+                'map' => null,
+                'mount' => array(),
+                'mung' => array(),
+                'extractable' => false,
+                'banner' => null,
+                'shebang' => '#!/usr/bin/env php',
+            ),
+        );
+
+        $stub = array(
+            'stub' => array(
+                'web' => null,
+            )
+        );
+
+        $this->assertEquals(
+            $expected,
+            $this->processor->processConfiguration(
+                $this->definition,
+                array($stub)
+            )
+        );
+    }
+
+    /**
+     * Make sure that the default source settings are set as expected.
+     */
+    public function testDefaultSource()
+    {
+        $expected = array(
+            'sources' => array(
+                'base' => '/path/to/base/directory',
+                'dirs' => array(
+                    array(
+                        'path' => 'path/to/directory',
+                        'extension' => array('php'),
+                        'filter' => null,
+                        'ignore' => array(),
+                        'rename' => null,
+                    ),
+                    array(
+                        'extension' => array('php'),
+                        'filter' => '/filter/',
+                        'ignore' => array(
+                            'directory/to/ignore/',
+                            'file/to/ignore.php',
+                        ),
+                        'path' => 'path/to/another/directory',
+                        'rename' => 'different/internal/path',
+                    ),
+                ),
+                'files' => array(
+                    array(
+                        'path' => 'path/to/file.php',
+                        'rename' => null,
+                    ),
+                    array(
+                        'path' => 'path/to/another/file.php',
+                        'rename' => 'different/internal/path.php',
+                    ),
+                ),
+            )
+        );
+
+        $sources = array(
+            'sources' => array(
+                'base' => '/path/to/base/directory',
+                'dirs' => array(
+                    'path/to/directory',
+                    array(
+                        'filter' => '/filter/',
+                        'ignore' => array(
+                            'directory/to/ignore/',
+                            'file/to/ignore.php',
+                        ),
+                        'path' => 'path/to/another/directory',
+                        'rename' => 'different/internal/path',
+                    ),
+                ),
+                'files' => array(
+                    'path/to/file.php',
+                    array(
+                        'path' => 'path/to/another/file.php',
+                        'rename' => 'different/internal/path.php',
+                    ),
+                ),
+            )
+        );
+
+        $this->assertEquals(
+            $expected,
+            $this->processor->processConfiguration(
+                $this->definition,
+                array($sources)
+            )
+        );
+    }
+
+    /**
+     * Make sure that the default stub settings are set as expected.
+     */
+    public function testDefaultStub()
+    {
+        $expected = array(
+            'stub' => array(
+                'banner' => <<<BANNER
+This is an example
+
+    banner that span
+
+        multiple lines
+BANNER
+,
+                'extractable' => true,
+                'intercept' => true,
+                'load' => array(
+                    array(
+                        'file' => '/path/to/test.phar',
+                        'alias' => null,
+                    ),
+                    array(
+                        'alias' => 'alias.phar',
+                        'file' => '/path/to/test.phar',
+                    ),
+                ),
+                'map' => 'test.phar',
+                'mount' => array(
+                    array(
+                        'external' => '/path/to/dir',
+                        'internal' => 'mount/point'
+                    ),
+                    array(
+                        'external' => '/path/to/file.php',
+                        'internal' => 'config/file.php'
+                    ),
+                ),
+                'mung' => array(
+                    'PHP_SELF',
+                ),
+                'require' => array(
+                    array(
+                        'file' => 'internal/path/to/file.php',
+                        'internal' => true,
+                    ),
+                    array(
+                        'file' => '/external/path/to/file.php',
+                        'internal' => false,
+                    ),
+                ),
+                'shebang' => '#!/usr/bin/php',
+                'source' => array(
+                    array(
+                        'source' => 'testFunc();',
+                        'after' => true,
+                    ),
+                    array(
+                        'after' => false,
+                        'source' => 'anotherFunc();',
+                    ),
+                ),
+                'web' => array(
+                    'alias' => 'test.phar',
+                    'not_found' => '404.php',
+                    'rewrite' => 'testRewrite',
+                    'index' => 'index.php',
+                    'mime' => array(
+                        'avi' => 'video/avi',
+                        'bmp' => 'image/bmp',
+                        'c' => 'text/plain',
+                        'c++' => 'text/plain',
+                        'cc' => 'text/plain',
+                        'cpp' => 'text/plain',
+                        'css' => 'text/css',
+                        'dtd' => 'text/plain',
+                        'gif' => 'image/gif',
+                        'h' => 'text/plain',
+                        'htm' => 'text/html',
+                        'html' => 'text/html',
+                        'htmls' => 'text/html',
+                        'ico' => 'image/x-ico',
+                        'inc' => Phar::PHP,
+                        'jpe' => 'image/jpeg',
+                        'jpeg' => 'image/jpeg',
+                        'jpg' => 'image/jpeg',
+                        'js' => 'application/x-javascript',
+                        'log' => 'text/plain',
+                        'mid' => 'audio/midi',
+                        'midi' => 'audio/midi',
+                        'mod' => 'audio/mod',
+                        'mov' => 'movie/quicktime',
+                        'mp3' => 'audio/mp3',
+                        'mpeg' => 'video/mpeg',
+                        'mpg' => 'video/mpeg',
+                        'pdf' => 'application/pdf',
+                        'php' => Phar::PHP,
+                        'phps' => Phar::PHPS,
+                        'png' => 'image/png',
+                        'rng' => 'text/plain',
+                        'swf' => 'application/shockwave-flash',
+                        'tif' => 'image/tiff',
+                        'tiff' => 'image/tiff',
+                        'txt' => 'text/plain',
+                        'wav' => 'audio/wav',
+                        'xbm' => 'image/xbm',
+                        'xml' => 'text/xml',
+                        'xsd' => 'text/plain',
+                    ),
+                ),
+            ),
+        );
+
+        $stub = array(
+            'stub' => array(
+                'banner' => <<<BANNER
+This is an example
+
+    banner that span
+
+        multiple lines
+BANNER
+,
+                'extractable' => true,
+                'intercept' => true,
+                'load' => array(
+                    array(
+                        'file' => '/path/to/test.phar',
+                    ),
+                    array(
+                        'alias' => 'alias.phar',
+                        'file' => '/path/to/test.phar',
+                    ),
+                ),
+                'map' => 'test.phar',
+                'mount' => array(
+                    array(
+                        'external' => '/path/to/dir',
+                        'internal' => 'mount/point'
+                    ),
+                    array(
+                        'external' => '/path/to/file.php',
+                        'internal' => 'config/file.php'
+                    ),
+                ),
+                'mung' => array(
+                    'PHP_SELF',
+                ),
+                'require' => array(
+                    array(
+                        'file' => 'internal/path/to/file.php',
+                    ),
+                    array(
+                        'file' => '/external/path/to/file.php',
+                        'internal' => false,
+                    ),
+                ),
+                'shebang' => '#!/usr/bin/php',
+                'source' => array(
+                    array(
+                        'source' => 'testFunc();',
+                    ),
+                    array(
+                        'after' => false,
+                        'source' => 'anotherFunc();',
+                    ),
+                ),
+                'web' => array(
+                    'alias' => 'test.phar',
+                    'not_found' => '404.php',
+                    'rewrite' => 'testRewrite',
+                ),
+            ),
+        );
+
+        $this->assertEquals(
+            $expected,
+            $this->processor->processConfiguration(
+                $this->definition,
+                array($stub)
+            )
+        );
+    }
+
+    /**
+     * Make sure that nothing is set if no configuration is given.
+     */
+    public function testEmptyConfig()
+    {
+        $this->assertEquals(
+            array(),
+            $this->processor->processConfiguration(
+                $this->definition,
+                array(
+                    array() // empty config
+                )
+            )
+        );
+    }
+
+    /**
+     * Sets up the definition and processor.
+     */
+    protected function setUp()
+    {
+        $this->definition = new Definition();
+        $this->processor = new Processor();
+    }
+}
