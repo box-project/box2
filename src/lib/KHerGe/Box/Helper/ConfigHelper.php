7758b9209c5679ce227b511f995318bc6e5374fa
Kevin Herrera <kevin@herrera.io>
2014-01-17 16:36:58 -0800

    Adding a configuration helper.

---
+++ src/lib/KHerGe/Box/Helper/ConfigHelper.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace KHerGe\Box\Helper;
+
+use InvalidArgumentException;
+use KHerGe\Box\Config\Definition;
+use KHerGe\Box\Config\Loader\AbstractFileLoader;
+use RuntimeException;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Console\Helper\Helper;
+
+/**
+ * Helps load configuration settings from a variety of sources.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class ConfigHelper extends Helper
+{
+    /**
+     * The configuration definition.
+     *
+     * @var Definition
+     */
+    private $definition;
+
+    /**
+     * The configuration definition processor.
+     *
+     * @var Processor
+     */
+    private $processor;
+
+    /**
+     * Initializes the configuration helper.
+     */
+    public function __construct()
+    {
+        $this->definition = new Definition();
+        $this->processor = new Processor();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'config';
+    }
+
+    /**
+     * Loads configuration settings from a directory or file.
+     *
+     * @param string $path The configuration directory or file path.
+     *
+     * @return array The configuration settings.
+     *
+     * @throws InvalidArgumentException If $path does not exist.
+     */
+    public function load($path = null)
+    {
+        if (null !== $path) {
+            if (is_dir($path)) {
+                /** @var DelegatingLoader $loader */
+                list($loader, $dir, $extensions) = $this->createLoader($path);
+            } elseif (is_file($path)) {
+                /** @var DelegatingLoader $loader */
+                $loader = $this->createLoader(dirname($path));
+                $loader = $loader[0];
+
+                return $this->processor->processConfiguration(
+                    $this->definition,
+                    array($loader->load(basename($path)))
+                );
+            } else {
+                throw new InvalidArgumentException(
+                    "The path \"$path\" does not exist."
+                );
+            }
+        } else {
+            list($loader, $dir, $extensions) = $this->createLoader();
+        }
+
+        $file = $this->findFile($dir, $extensions);
+
+        return $this->processor->processConfiguration(
+            $this->definition,
+            array($loader->load($file))
+        );
+    }
+
+    /**
+     * Creates a new configuration loader.
+     *
+     * @param string $dir The configuration directory path.
+     *
+     * @return array The loader, configuration directory path, and supported
+     *               file extensions.
+     */
+    private function createLoader($dir = null)
+    {
+        if (null === $dir) {
+            $dir = realpath('.');
+        }
+
+        $extensions = array();
+        $locator = new FileLocator($dir);
+        $loaders = array(
+            '\KHerGe\Box\Config\Loader\JsonFileLoader' => null,
+            '\KHerGe\Box\Config\Loader\PhpFileLoader' => null,
+            '\KHerGe\Box\Config\Loader\YamlFileLoader' => null,
+        );
+
+        /** @var AbstractFileLoader $loader */
+        foreach ($loaders as $class => &$loader) {
+            $loader = new $class($locator);
+            $extensions = array_merge($extensions, $loader->getSupportedExtensions());
+        }
+
+        return array(
+            new DelegatingLoader(new LoaderResolver($loaders)),
+            $dir,
+            $extensions
+        );
+    }
+
+    /**
+     * Finds the configuration file to load.
+     *
+     * @param string $dir        The configuration directory path.
+     * @param array  $extensions The supported file extensions.
+     *
+     * @return string The configuration file path.
+     *
+     * @throws RuntimeException If the configuration file could not be found,
+     *                          or if too many configuration files were found.
+     */
+    private function findFile($dir, array $extensions)
+    {
+        $found = array(
+            'dist' => array(),
+            'normal' => array(),
+        );
+
+        foreach ($extensions as $extension) {
+            $file = $dir . DIRECTORY_SEPARATOR . 'box.' . $extension;
+
+            if (file_exists($file)) {
+                $found['normal'][] = $file;
+            } elseif (file_exists($file . '.dist')) {
+                $found['dist'][] = $file . '.dist';
+            }
+        }
+
+        if (empty($found['normal'])) {
+            if (empty($found['dist'])) {
+                throw new RuntimeException(
+                    "The directory \"$dir\" did not contain the configuration file."
+                );
+            } elseif (count($found['dist']) > 1) {
+                throw new RuntimeException(
+                    "Many configuration files were found in \"$dir\". You will need to specify the one you want to use."
+                );
+            }
+
+            return $found['dist'][0];
+        } elseif (count($found['normal']) > 1) {
+            throw new RuntimeException(
+                "Many configuration files were found in \"$dir\". You will need to specify the one you want to use."
+            );
+        }
+
+        return $found['normal'][0];
+    }
+}
---
+++ src/tests/KHerGe/Box/Tests/Unit/Helper/ConfigHelperTest.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace KHerGe\Box\Tests\Unit\Helper;
+
+use KHerGe\Box\Helper\ConfigHelper;
+use Phine\Test\Temp;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Performs unit testing on `ConfigHelper`.
+ *
+ * @see KHerGe\Box\Helper\ConfigHelper
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class ConfigHelperTest extends TestCase
+{
+    /**
+     * The configuration helper instance being tested.
+     *
+     * @var ConfigHelper
+     */
+    private $helper;
+
+    /**
+     * The temporary directory path.
+     *
+     * @var string
+     */
+    private $dir;
+
+    /**
+     * The previous directory path.
+     *
+     * @var string
+     */
+    private $previous;
+
+    /**
+     * The temporary file manager.
+     *
+     * @var Temp
+     */
+    private $temp;
+
+    /**
+     * Make sure the expected helper name is returned.
+     */
+    public function testGetName()
+    {
+        $this->assertEquals(
+            'config',
+            $this->helper->getName(),
+            'The expected helper name should be returned.'
+        );
+    }
+
+    /**
+     * Make sure that we can load the default configuration file.
+     */
+    public function testLoad()
+    {
+        file_put_contents("{$this->dir}/box.json.dist", '{"compression":"GZ"}');
+
+        $this->assertEquals(
+            array(
+                'compression' => 'GZ',
+                'mode' => 644,
+                'output' => 'output.phar',
+                'signature' => array(
+                    'type' => 'SHA1',
+                ),
+            ),
+            $this->helper->load(),
+            'The distribution configuration file should be loaded.'
+        );
+
+        file_put_contents("{$this->dir}/box.json", '{}');
+
+        $this->assertEquals(
+            array(
+                'compression' => 'NONE',
+                'mode' => 644,
+                'output' => 'output.phar',
+                'signature' => array(
+                    'type' => 'SHA1',
+                ),
+            ),
+            $this->helper->load(),
+            'The user configuration file should be loaded.'
+        );
+    }
+
+    /**
+     * Make sure that we can load from a different directory.
+     */
+    public function testLoadDir()
+    {
+        $dir = $this->temp->createDir();
+
+        file_put_contents("$dir/box.json", '{}');
+
+        $this->assertEquals(
+            array(
+                'compression' => 'NONE',
+                'mode' => 644,
+                'output' => 'output.phar',
+                'signature' => array(
+                    'type' => 'SHA1',
+                ),
+            ),
+            $this->helper->load($dir),
+            'The default configuration file should be loaded.'
+        );
+    }
+
+    /**
+     * Make sure that we can specify the file to load.
+     */
+    public function testLoadFile()
+    {
+        file_put_contents("{$this->dir}/alt.json", '{}');
+
+        $this->assertEquals(
+            array(
+                'compression' => 'NONE',
+                'mode' => 644,
+                'output' => 'output.phar',
+                'signature' => array(
+                    'type' => 'SHA1',
+                ),
+            ),
+            $this->helper->load('alt.json'),
+            'The default configuration file should be loaded.'
+        );
+    }
+
+    /**
+     * Make sure that an invalid path throws an exception.
+     */
+    public function testLoadInvalidPath()
+    {
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            'The path "/should/not/exist" does not exist.'
+        );
+
+        $this->helper->load('/should/not/exist');
+    }
+
+    /**
+     * Make sure that an exception is thrown if the default file could not be found.
+     */
+    public function testLoadDefaultMissing()
+    {
+        $this->setExpectedException(
+            'RuntimeException',
+            "The directory \"{$this->dir}\" did not contain the configuration file."
+        );
+
+        $this->helper->load();
+    }
+
+    /**
+     * Make sure that an exception is thrown for too many default .dist files.
+     */
+    public function testLoadManyDist()
+    {
+        touch("{$this->dir}/box.json.dist");
+        touch("{$this->dir}/box.yaml.dist");
+
+        $this->setExpectedException(
+            'RuntimeException',
+            "Many configuration files were found in \"{$this->dir}\". You will need to specify the one you want to use."
+        );
+
+        $this->helper->load();
+    }
+
+    /**
+     * Make sure that an exception is thrown for too many default files.
+     */
+    public function testLoadManyNormal()
+    {
+        touch("{$this->dir}/box.json");
+        touch("{$this->dir}/box.yaml");
+
+        $this->setExpectedException(
+            'RuntimeException',
+            "Many configuration files were found in \"{$this->dir}\". You will need to specify the one you want to use."
+        );
+
+        $this->helper->load();
+    }
+
+    /**
+     * @override
+     */
+    protected function setUp()
+    {
+        $this->helper = new ConfigHelper();
+        $this->previous = realpath('.');
+        $this->temp = new Temp();
+        $this->dir = $this->temp->createDir();
+
+        chdir($this->dir);
+    }
+
+    /**
+     * @override
+     */
+    protected function tearDown()
+    {
+        chdir($this->previous);
+
+        $this->temp->purgePaths();
+    }
+}
